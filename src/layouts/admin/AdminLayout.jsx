be13dea1341c370bc712f07a321991f41b7142a0
Thanarat Phonbunyawat <Thanarat.phol55@gmail.com>
2026-09-10 22:27:44 +0700

    Dashboard final

--- src/layouts/admin/AdminLayout.jsx
+++ src/layouts/admin/AdminLayout.jsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Sidebar } from './Sidebar';
+import { useWorkerStore } from '../../store/useWorkerStore';
+import { useWorkLogStore } from '../../store/useWorkLogStore';
 
 export function AdminLayout({ children }) {
+  const fetchWorkers = useWorkerStore((s) => s.fetchWorkers);
+  const workersLoaded = useWorkerStore((s) => s.loaded);
+  const fetchEntries = useWorkLogStore((s) => s.fetchEntries);
+  const entriesLoaded = useWorkLogStore((s) => s.loaded);
+
+  useEffect(() => {
+    if (!workersLoaded) fetchWorkers();
+    if (!entriesLoaded) fetchEntries();
+  }, [fetchWorkers, workersLoaded, fetchEntries, entriesLoaded]);
+
   return (
     <div className="flex h-screen bg-farm-bg">
       <Sidebar />
